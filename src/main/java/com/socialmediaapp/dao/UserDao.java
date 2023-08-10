25041dd13a74c47712dced15c12e63b4661970df
Prashant <pashu.barde@gmail.com>
2023-08-10 16:14:03 +0530

    created user class, userDao, userController

---
+++ src/main/java/com/socialmediaapp/dao/UserDao.java
@@ -0,0 +1,26 @@
+package com.socialmediaapp.dao;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.socialmediaapp.bean.User;
+
+@Component
+public class UserDao {
+
+	private static List<User> users = new ArrayList<User>();
+	
+	static {
+		users.add(new User(1, "Prashant", LocalDate.now().minusYears(30)));
+		users.add(new User(2, "Ashok", LocalDate.now().minusYears(60)));
+		users.add(new User(3, "Pankaj", LocalDate.now().minusYears(32)));
+		users.add(new User(4, "Amit", LocalDate.now().minusYears(25)));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+}
