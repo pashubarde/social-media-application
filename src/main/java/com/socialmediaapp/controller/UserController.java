0d89ae55a7cfe9ba4311ee3bfbaa788fc49ed6dd
Prashant <pashu.barde@gmail.com>
2023-08-10 19:13:59 +0530

    created-post-user-api

--- src/main/java/com/socialmediaapp/controller/UserController.java
+++ src/main/java/com/socialmediaapp/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.socialmediaapp.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.socialmediaapp.bean.User;
 import com.socialmediaapp.dao.UserDao;
@@ -12,19 +17,27 @@ import com.socialmediaapp.dao.UserDao;
 @RestController
 public class UserController {
 	private UserDao dao;
-	
+
 	public UserController(UserDao dao) {
 		super();
 		this.dao = dao;
 	}
 
 	@GetMapping(path = "/users")
-	public List<User> retrieveAllUsers(){
+	public List<User> retrieveAllUsers() {
 		return dao.findAll();
 	}
-	
+
 	@GetMapping(path = "/users/{id}")
-	public User retrieveUsers(@PathVariable int id){
+	public User retrieveUsers(@PathVariable int id) {
 		return dao.findUsers(id);
+	}
+
+	@PostMapping(path = "/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = dao.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
